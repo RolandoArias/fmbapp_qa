a001f9f48b11662894ea91e3c8de04fe7d9c0f79
Rolando Arias <rolando@skywebplus.com>
2018-12-26 13:18:07 -0600

    Update EscuelaEmpresa.php

--- app/EscuelaEmpresa.php
+++ app/EscuelaEmpresa.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class EscuelaEmpresa extends Model
 {
-    protected $table = "Escuela-Empresa";
+    protected $table = "escuela-empresa";
 
     protected $fillable = [
         'id', 'crmit_claveescuelaempresacrm', 'TerritoryId', 'Name', 'crmit_calidadid', 'crmit_descturno1', 'crmit_descturno2', 'crmit_descturno3'
